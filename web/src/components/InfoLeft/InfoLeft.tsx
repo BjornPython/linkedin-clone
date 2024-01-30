31906301a4fbb663d87ab4f0a34c998ae8749d8e
nathan <nathanflores887@gmail.com>
2024-01-30 15:12:23 +0800

    feat(): move info left code to sepeerate component

---
+++ web/src/components/InfoLeft/InfoLeft.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react'
+
+import banner from 'public/images/banner.png'
+import dp from 'public/images/dp.jpg'
+
+import { Link } from '@redwoodjs/router'
+
+const InfoLeft = () => {
+  const [scrollVal, setSCrollVal] = useState(0)
+
+  const [isFixed, setIsFixed] = useState(false)
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      setSCrollVal(window.scrollY)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (scrollVal >= 390 && !isFixed) {
+      setIsFixed(true)
+    } else if (scrollVal <= 390 && isFixed) {
+      setIsFixed(false)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollVal])
+
+  return (
+    <div className="infoLeftContainer">
+      <div className="leftProfileContainer">
+        <img src={banner} alt="" className="userBanner" />
+        <div className="left-info">
+          <Link to={''}>
+            <img src={dp} className="newsfeed-dp" alt="" />
+          </Link>
+
+          <p className="newsfeed-username">Nathan Flores</p>
+
+          <p className="newsfeed-bio">
+            Web Developer | React | JavaScript | NodeJS | ExpressJS | MongoDB |
+            Firebase
+          </p>
+
+          <div className="connection-container">
+            <p className="connections">Connections</p>
+            <p className="grow-ur-network">Grow your network</p>
+          </div>
+
+          <div className="access-container">
+            <p className="access-one">Access exlusive tools & insights</p>
+            <p className="access-two">Grow your network</p>
+          </div>
+
+          <div className="my-items-div">
+            <p>My items</p>
+          </div>
+        </div>
+      </div>
+      <div className={`discover-container ${isFixed && 'discover-div-fixed'}`}>
+        <p>Groups</p>
+        <p>Events</p>
+        <p>Followed Hashtags</p>
+
+        <div className="discover-div">
+          <p id="discover-more">Discover more</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InfoLeft
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,43 +1,10 @@
-import banner from 'public/images/banner.png'
-import dp from 'public/images/dp.jpg'
-
-import { Link } from '@redwoodjs/router'
+import InfoLeft from 'src/components/InfoLeft/InfoLeft'
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="newsfeed">
-        <div className="infoLeftContainer">
-          <div className="leftProfileContainer">
-            <img src={banner} alt="" className="userBanner" />
-            <div className="left-info">
-              <Link to={''}>
-                <img src={dp} className="newsfeed-dp" alt="" />
-              </Link>
-
-              <p className="newsfeed-username">Nathan Flores</p>
-
-              <p className="newsfeed-bio">
-                Web Developer | React | JavaScript | NodeJS | ExpressJS |
-                MongoDB | Firebase
-              </p>
-
-              <div className="connection-container">
-                <p className="connections">Connections</p>
-                <p className="grow-ur-network">Grow your network</p>
-              </div>
-
-              <div className="access-container">
-                <p className="access-one">Access exlusive tools & insights</p>
-                <p className="access-two">Grow your network</p>
-              </div>
-
-              <div className="my-items-div">
-                <p>My items</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <InfoLeft />
 
         {/* <Posts postIds={postIds} ENDPOINT={process.env.API_ENDPOINT} /> */}
       </div>
