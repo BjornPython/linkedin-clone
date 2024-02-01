70133a5de8de14cfdecb619d292628a1dfd6bd00
nathan <nathanflores887@gmail.com>
2024-02-01 14:13:26 +0800

    feat(): init message page comps

--- web/src/pages/MessagesPage/MessagesPage.tsx
+++ web/src/pages/MessagesPage/MessagesPage.tsx
@@ -1,20 +1,15 @@
-import { Link, routes } from '@redwoodjs/router'
-import { Metadata } from '@redwoodjs/web'
-
 const MessagesPage = () => {
   return (
-    <>
-      <Metadata title="Messages" description="Messages page" />
+    <div className="messagePage">
+      <div className="messageContent">
+        <div className="messageContainer">
+          {/* <Chats  /> */}
+          {/* <UserChat  /> */}
+        </div>
 
-      <h1>MessagesPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/MessagesPage/MessagesPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>messages</code>, link to me with `
-        <Link to={routes.messages()}>Messages</Link>`
-      </p>
-    </>
+        <div className="messageAd">{/* <MessageAd /> */}</div>
+      </div>
+    </div>
   )
 }
 
