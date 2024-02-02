f8ff3615cbd429a488772fb5c6e269be7fadc1bd
nathan <nathanflores887@gmail.com>
2024-02-02 14:18:59 +0800

    feat(): create unique class name for notif page

--- web/src/components/NotifLeftBanner/NotifLeftBanner.tsx
+++ web/src/components/NotifLeftBanner/NotifLeftBanner.tsx
@@ -1,6 +1,6 @@
 const NotifLeftBanner = () => {
   return (
-    <div className="leftContainer">
+    <div className="notifLeftContainer">
       <div className="notifLeftBanner">
         <p>Manage your Notifications</p>
         <p className="viewSettings">View Settings</p>
