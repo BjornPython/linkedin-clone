043755f14728256d67ee533f50b6148cd941bd59
nathan <nathanflores887@gmail.com>
2024-03-06 14:58:58 +0800

    refactor(): remove fixed banners and improve chat messages

--- web/src/lib/data.ts
+++ web/src/lib/data.ts
@@ -157,25 +157,24 @@ export const sampleConvo = [
   },
   {
     sender: 'Nathan Flores',
-    message: "Thank you, Maximo. I'm always open to new opportunities!",
+    message: 'Thank you, Maximo. I appreciate it!',
     dp: myDp,
   },
   {
     sender: 'Maximo Guzman',
     message:
-      "That's great to hear, Nathan. In my experience, being disciplined, coachable, and passionate about the industry are some of the most important traits for a developer. Would you agree?",
+      'In my experience, being disciplined and passionate about the industry are some of the most important traits for a developer. Would you agree?',
     dp: sampleChatDp,
   },
   {
     sender: 'Nathan Flores',
-    message:
-      'Absolutely. I think discipline is important because it helps you stay focused and meet deadlines, while being coachable allows you to learn from your mistakes and continuously improve. And of course, being passionate about the industry helps you stay motivated and engaged in your work.',
+    message: 'Absolutely.',
     dp: myDp,
   },
   {
     sender: 'Maximo Guzman',
     message:
-      'Well said, Nathan. You seem to embody all of those traits, which is why I think you would be a valuable member of a company. Have you ever considered working for a different company?',
+      'You seem to embody all of those traits, which is why I think you would be a valuable member of our company.',
     dp: sampleChatDp,
   },
   {
